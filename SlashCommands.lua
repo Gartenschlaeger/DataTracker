ade0b46fe70b653b8c617549f9cac1d2cc5997e2
Gartenschlaeger <mail@kaisnet.de>
2022-06-18 22:15:40 +0200

    add ui debug slash command

--- SlashCommands.lua
+++ SlashCommands.lua
@@ -1,22 +1,21 @@
 SLASH_DT_SHOW1 = "/dts"
-SLASH_DT_HIDE1 = "/dth"
-SLASH_DT_LOGLEVEL1 = "/dtll"
-
 function DT_SlashCommand_Show()
     DT_LogTrace('DT_SlashCommand_Show')
-
+    
     DT_DatabaseBrowser:Show()
 end
 
+SLASH_DT_HIDE1 = "/dth"
 function DT_SlashCommand_Hide()
     DT_LogTrace('DT_SlashCommand_Hide')
     
     DT_DatabaseBrowser:Hide()
 end
 
+SLASH_DT_LOGLEVEL1 = "/dtll"
 function DT_SlashCommand_LogLevel(msg)
     DT_LogTrace('DT_SlashCommand_DebugLogs', msg)
-
+    
     local logLevel = tonumber(msg)
     for n, v in pairs(DT_LogLevel) do
         if (logLevel == n or logLevel == v) then
@@ -26,8 +25,15 @@ function DT_SlashCommand_LogLevel(msg)
     end
 end
 
+SLASH_DT_UIDEBUG1 = '/dtuidebug'
+function DT_SlashCommand_UIDebug()
+    LoadAddOn('Blizzard_DebugTools')
+    FrameStackTooltip_Toggle()
+end
+
 function DT_InitSlashCommands()
     SlashCmdList.DT_SHOW = DT_SlashCommand_Show
     SlashCmdList.DT_HIDE = DT_SlashCommand_Hide
     SlashCmdList.DT_LOGLEVEL = DT_SlashCommand_LogLevel
+    SlashCmdList.DT_UIDEBUG = DT_SlashCommand_UIDebug
 end
