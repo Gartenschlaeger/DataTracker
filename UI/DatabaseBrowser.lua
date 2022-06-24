f4240a81e9b8931678626dd99fd34f81dafc47ab
Gartenschlaeger <mail@kaisnet.de>
2022-06-24 13:39:40 +0200

    database browser enhancements

--- UI/DatabaseBrowser.lua
+++ UI/DatabaseBrowser.lua
@@ -1,8 +1,13 @@
-function DT_DatabaseBrowser_OnLoad()
+function DT_DatabaseBrowser_OnLoad(self)
     DataTracker:LogTrace('DT_DatabaseBrowser_OnLoad')
 
     DT_DatabaseBrowser.Title:SetText('DataTracker')
     DT_DatabaseBrowser:RegisterForDrag("LeftButton")
+
+    -- disable by default
+    for i = 1, 25 do
+        _G['DT_DatabaseBrowser_Entry' .. i]:Disable()
+    end
 
     DT_DatabaseBrowser_ScrollBar:Show()
 end
@@ -21,15 +26,15 @@ function DT_DatabaseBrowser_OnDragStop(self)
     self:StopMovingOrSizing()
 end
 
-function DT_DatabaseBrowser_OnShow()
+function DT_DatabaseBrowser_OnShow(self)
     DataTracker:LogTrace('DT_DatabaseBrowser_OnShow')
 end
 
-function DT_DatabaseBrowser_OnHide()
+function DT_DatabaseBrowser_OnHide(self)
     DataTracker:LogTrace('DT_DatabaseBrowser_OnHide')
 end
 
-function DT_DatabaseBrowser_OnSearch()
+function DT_DatabaseBrowser_OnSearch(self)
     DataTracker:LogTrace('DT_DatabaseBrowser_OnSearch')
 
     DT_SearchResults = {}
@@ -40,11 +45,18 @@ function DT_DatabaseBrowser_OnSearch()
 
     if (searchText and strlen(searchText) > 0) then
         for itemId, itemInfo in pairs(DT_ItemDb) do
-            local nameMatches = strfind(strlower(itemInfo['nam']), searchText, 1, true)
+            local nameMatches = strfind(strlower(itemInfo.nam), searchText, 1, true)
             if (nameMatches) then
                 local result = {}
-                result['itemId'] = itemId
-                result['itemName'] = itemInfo['nam']
+                result.itemId = itemId
+                result.itemName = itemInfo.nam
+
+                if (itemInfo.qlt) then
+                    local icR, icG, icB = GetItemQualityColor(itemInfo.qlt)
+                    result.color = { r = icR, g = icG, b = icB }
+                else
+                    result.color = { r = 1, g = 1, b = 1 }
+                end
 
                 DT_SearchResults[index] = result
                 index = index + 1
@@ -64,7 +76,7 @@ DT_SearchResults = {}
 local DT_RESULT_ITEMS_COUNT = 25
 local DT_RESULT_PIXEL_HEIGHT = 18
 
-function DT_DatabaseBrowser_ScrollBar_Update()
+function DT_DatabaseBrowser_ScrollBar_Update(self)
     DataTracker:LogTrace('DT_DatabaseBrowser_ScrollBar_Update')
 
     local totalResults = DataTracker:GetTableSize(DT_SearchResults)
@@ -74,19 +86,29 @@ function DT_DatabaseBrowser_ScrollBar_Update()
 	for i = 1, DT_RESULT_ITEMS_COUNT do
         local result = DT_SearchResults[i + offset]
 
-        local indexString = _G["DT_DatabaseBrowser_Entry"..i..'Index']
-        local valueString = _G["DT_DatabaseBrowser_Entry"..i..'Value']
-        local nameString = _G["DT_DatabaseBrowser_Entry"..i..'Name']
+        local btn = _G['DT_DatabaseBrowser_Entry' .. i]
+        btn:SetAttribute('itemIndex', i + offset)
+        btn:SetScript("OnClick", function(self)
+            local itemIndex = tonumber(self:GetAttribute('itemIndex'))
+            local result = DT_SearchResults[itemIndex]
+
+            -- TODO: show where the item can be found (zone, unit, chance)
+            DataTracker:LogDebug(result.itemName, result.itemId)
+        end)
+
+        local fsVal1 = _G["DT_DatabaseBrowser_Entry" .. i .. 'Val1']
+        local fsVal2 = _G["DT_DatabaseBrowser_Entry" .. i .. 'Val2']
+        local fsVal3 = _G["DT_DatabaseBrowser_Entry" .. i .. 'Val3']
 
         if (result) then
-            indexString:SetText(result.itemName)
-            valueString:SetText(result.itemId)
-            nameString:SetText('') -- TODO: zone name
+            btn:Enable()
+            fsVal1:SetText(result.itemName)
+            fsVal1:SetTextColor(result.color.r, result.color.g, result.color.b)
         else
-            indexString:SetText('')
-            valueString:SetText('')
-            nameString:SetText('')
+            btn:Disable()
+            fsVal1:SetText('')
+            fsVal2:SetText('')
+            fsVal3:SetText('')
         end
-
 	end
 end
